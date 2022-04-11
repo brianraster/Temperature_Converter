fcfb0c7b1ab89ac71cfe9cad01cee505b9dae358
Brian Aster <brianaster85@gmail.com>
2022-04-11 12:04:15 -0500

    Initial

---
+++ src/logic/Converter.java
@@ -0,0 +1,35 @@
+package logic;
+
+public class Converter {
+    float temp;
+
+    public double FToC (double temp) {
+        temp = (temp - 32) * (5/9);
+        return temp;
+    }
+
+    public double FToK (double temp) {
+        temp = CToK(FToC(temp));
+        return temp;
+    }
+
+    public double CToF (double temp) {
+        temp = temp * (9/5) + 32;
+        return temp;
+    }
+
+    public double CToK (double temp) {
+        temp = temp + 273.15;
+        return temp;
+    }
+
+    public double KToC (double temp) {
+        temp = temp - 273.15;
+        return temp;
+    }
+
+    public double KToF (double temp) {
+        temp = CToF(KToC(temp));
+        return temp;
+    }
+}
